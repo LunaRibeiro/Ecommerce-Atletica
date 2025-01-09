e9f002b156ea30e2bbae240afab222ff0b6af655
LunaRibeiro <lunasantos@alunos.utfpr.edu.br>
2025-01-08 22:03:19 -0300

    Conectando o Banco de Dados

---
+++ BDEcommerceAtletica/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // String de conexão com o MySQL
+        string connectionString = "Server=localhost; Database=BDEcommerceAtletica; Uid=root; Pwd=utfpr;";
+
+        // Estabelecer a conexão
+        using (MySqlConnection connection = new MySqlConnection(connectionString))
+        {
+            try
+            {
+                connection.Open();  // Abrir a conexão
+                Console.WriteLine("Conexão com MySQL bem-sucedida!");
+
+                // Exemplo de comando SQL: Listar as tabelas no banco de dados
+                string sqlQuery = "SHOW TABLES;";
+                MySqlCommand command = new MySqlCommand(sqlQuery, connection);
+
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader.GetString(0)); // Exibir nome das tabelas
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro ao conectar: " + ex.Message);
+            }
+        }
+    }
+}
