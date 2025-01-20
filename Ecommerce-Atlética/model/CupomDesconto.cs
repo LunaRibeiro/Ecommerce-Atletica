f947438ee1d9cde168fd606d257161a6ff7fce09
Lucas-Verza <lucasverza@alunos.utfpr.edu.br>
2025-01-20 16:46:00 -0300

    Pacote model e classes

---
+++ Ecommerce-Atlética/model/CupomDesconto.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce_Atlética.model
+{
+    public class CupomDesconto
+    {
+            public int Id { get; set;}
+            public int IdCliente { get; set;}
+            public bool TipoCliente { get; set; }
+            public double ValorDesconto { get; set; } = 0;
+
+
+            
+            public double GerarDesconto(string categoriaProduto, bool TipoCliente, /*Valor do produto sendo usado para retornar 
+            diretamente o valor do produto com desconto caso existe*/double valorProduto)
+            {
+                if (TipoCliente == true){
+                    //Switch com valor de desconto para cada categoria de produto
+                }
+                return valorProduto;
+            }
+
+    }
+}
